4d5336fa1b93fa46e53547dc82fd0b5d1bb2052f
Bas Dijkstra <bas@ontestautomation.com>
2019-08-26 16:12:34 +0200

    Initial commit

---
+++ testproject-specflow/StepDefinitions/ReturningLocationDataBasedOnCountryAndZipCodeSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace testproject_specflow.StepDefinitions
+{
+    [Binding]
+    public class ReturningLocationDataBasedOnCountryAndZipCodeSteps
+    {
+        [Given(@"the country code us and zip code (.*)")]
+        public void GivenTheCountryCodeUsAndZipCode(int p0)
+        {
+        }
+        
+        [When(@"I request the locations corresponding to these codes")]
+        public void WhenIRequestTheLocationsCorrespondingToTheseCodes()
+        {
+        }
+        
+        [Then(@"the response contains the place name Beverly Hills")]
+        public void ThenTheResponseContainsThePlaceNameBeverlyHills()
+        {
+        }
+        
+        [Then(@"the response contains exactly (.*) location")]
+        public void ThenTheResponseContainsExactlyLocation(int p0)
+        {
+        }
+        
+        [Then(@"the response has status code (.*)")]
+        public void ThenTheResponseHasStatusCode(int p0)
+        {
+        }
+    }
+}
